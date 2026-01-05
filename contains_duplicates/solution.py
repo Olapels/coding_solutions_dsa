20eaebdec294194e7f05e0a30429ee828ff44e0f
olapels <martinsajakaye53@yahoo.com>
2026-01-05 22:51:17 +0000

    added solution to contain duplicates

---
+++ contains_duplicates/solution.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        #initialize an empty set
+        set_array = set()
+        for num in nums:
+            #break out early if duplicate is found
+            if num in set_array:
+                return True
+            set_array.add(num)
+        #just return false after adding all values if no duplicate found
+        return False
+            
