0153b827e7d9be40ef6c22e2b6975533536bb97d
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-01-17 12:58:32 +0000

    Time: 3 ms (3.99%) | Memory: 19.2 MB (18.81%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        pointer = 0
+        n = len(nums)
+        
+        while pointer < n:
+            #if the current value at nums is the same as val
+            if nums[pointer] == val:
+                # Replace it with the last value from nums
+                nums[pointer] = nums[n - 1]
+                #reduce the size of what we check
+                n -= 1
+                
+            else:
+                # if the value is not the same, just skip and move forward
+                pointer += 1
+                
+        return n
