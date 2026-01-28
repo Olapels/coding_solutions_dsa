42303d1f0ac74293e55907972993030b162d14d6
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-01-28 22:25:12 +0000

    Time: 29 ms (44.98%) | Memory: 20.3 MB (37.80%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,11 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
+        #maximum steps that can be taken
         jump_reach = 0
 
         for i in range(len(nums)):
                 if i <= jump_reach:
+                    # i = how previous steps have been taken
+                    # nums[i] = how much further can i go
                     jump_reach = max(jump_reach, i + nums[i])
                 else:
+                    #jump reach is greater, we can't move forward
                     return False
         return True
         
