e3b4e1bfa9f70c8c1d9e62ee3a61f4ea7f9e7c13
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-03-24 06:37:39 +0000

    Time: 0 ms (100.00%) | Memory: 26.4 MB (96.83%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,21 +1,7 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        """
-        Do not return anything, modify nums in-place instead
-        """
         n = len(nums)
-        if n == 0:
-            return
-        
+        if n == 0: return
         k %= n
-
-        if k==0:
-            return
-            
-        new_nums = [0] * n
-        #first k elements on new list from the back k element
-        new_nums[:k] = nums[-k:]
-        #onward from position k up till the -k
-        new_nums[k:] = nums[:-k]
-
-        nums[:] = new_nums
+        if k == 0: return
+        nums[:] = nums[-k:] + nums[:-k]
