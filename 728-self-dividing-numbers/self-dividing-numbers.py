0234cfe7e9952f99d91b7f4196743fa7dfa29790
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-04-19 00:18:29 +0100

    Time: 15 ms (16.45%) | Memory: 19.2 MB (94.44%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.py
+++ 728-self-dividing-numbers/self-dividing-numbers.py
@@ -1,7 +1,6 @@
 class Solution:
     def selfDividingNumbers(self, left: int, right: int) -> List[int]:
         div_num = []
-
         for x in range(left,right+1):
             if all(int(d) != 0 and x % int(d) == 0 for d in str(x)):
                 div_num.append(x)
