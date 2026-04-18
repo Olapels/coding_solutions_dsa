ae8d3a325c406356519171c496ac8dc3072a0310
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-04-18 23:41:37 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.83%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        balanced = 0
+        split = 0
+        for x in range(len(s)):
+            if s[x] =='L':
+                balanced+=1
+            else:
+                balanced-=1
+            if balanced==0:
+               split+=1 
+        return split 
+        
