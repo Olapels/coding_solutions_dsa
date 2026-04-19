cce30c7d35946f9e15ff807782a3a52781415578
Martins Ajakaye <41131901+Olapels@users.noreply.github.com>
2026-04-20 00:03:06 +0100

    Time: 3 ms (95.30%) | Memory: 20.2 MB (49.29%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -2,6 +2,6 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         sum_val =[]
         for i in range(n+1):
-            sum_val.append(sum(int(d) for d in bin(i)[2:]))
+            sum_val.append(i.bit_count())
         return sum_val
         
